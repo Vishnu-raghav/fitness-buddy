8ba719d0ab0b78f191f22438147d85c3c59ce39a
Vishnu-raghav <vishnur1225@gmail.com>
2025-04-17 22:19:51 +0530

    set up routers

--- Backend/src/routes/user.routes.js
+++ Backend/src/routes/user.routes.js
@@ -1,5 +1,5 @@
 import {Router} from "express"
-import {loginUser, registerUser, logoutUser} from "../controllers/user.controllers.js"
+import {loginUser, registerUser, logoutUser, refreshAccessToken, changeCurrentPassword, getCurrentUser, updateAccountDetail, updateUserGoal} from "../controllers/user.controllers.js"
 import {verifyjwt} from "../middlewares/auth.middleware.js"
 const router = Router()
 
@@ -10,4 +10,9 @@ router.route("/login").post(loginUser)
 // secure routes
 
 router.route("/logout").post(verifyjwt, logoutUser)
+router.route("/refresh-token").post(refreshAccessToken)
+router.route("/change-password").post(verifyjwt,changeCurrentPassword)
+router.route("/current-user").get(verifyjwt,getCurrentUser)
+router.route("/update-account").patch(verifyjwt,updateAccountDetail)
+router.route("/update-goal").patch(verifyjwt,updateUserGoal)
 export default router
