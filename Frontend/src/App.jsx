e0af96e1f17734b3bd1dd9fc389665b22cfe634a
Vishnu-raghav <vishnur1225@gmail.com>
2025-04-19 00:57:14 +0530

    setup frintend

---
+++ Frontend/src/App.jsx
@@ -0,0 +1,12 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+   <h1 className='bg-amber-300'>fitness buddy</h1>
+  )
+}
+
+export default App
