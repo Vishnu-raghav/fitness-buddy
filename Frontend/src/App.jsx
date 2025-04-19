974b1c8c1e212487b8e1fe5696b5e051b0c64ad6
Vishnu-raghav <vishnur1225@gmail.com>
2025-04-19 22:49:26 +0530

    work on frontend

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,11 +1,34 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import './App.css'
-
+import axios from "axios"
 function App() {
-  const [count, setCount] = useState(0)
-
+  const [allgoals, setAllGoals] = useState()
+  const [show,setshow] = useState(false)
+const handler = (e) => {
+  setshow(true)
+  useEffect(() =>{
+    axios.get("http://localhost:8000/api/v1/users/goals")
+    .then((res) => {
+      console.log(res.data.data);
+      setAllGoals(res.data.data)
+    })
+    .catch((er)=>{
+      console.log(er)
+    })
+  },[])
+}
   return (
-   <h1 className='bg-amber-300'>fitness buddy</h1>
+   <>
+   <h1>{allgoals?.[0]?.name}</h1>
+   {allgoals?.map((data)=>(
+    <div
+    key={data._id}
+    >
+      <h2>{data.name}</h2>
+    </div>
+   ))}
+   </>
+
   )
 }
 
