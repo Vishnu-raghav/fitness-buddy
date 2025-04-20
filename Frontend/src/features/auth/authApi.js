8cf01edf18dca2e0d2dd14b432e4f88d9ae089be
Vishnu-raghav <vishnur1225@gmail.com>
2025-04-20 16:45:00 +0530

    api + statemanagement

---
+++ Frontend/src/features/auth/authApi.js
@@ -0,0 +1,20 @@
+import axios, { Axios } from "axios"
+
+const Api = Axios.create({
+    baseUrl : "http://localhost:5000/api/v1",
+    withCredentials : true
+})
+
+export const registerUser = (data) => Api.post("/register",data)
+
+export const loginUser = (data) => Api.post("/login",data)
+
+export const logoutUser = () => Api.post("/logout")
+
+export const getCurrentUser = () => Api.get("/current-user")
+
+export const changePassword = (data) => Api.put("/change-password",data)
+
+export const updateAccount = (data) => Api.put("/update-account",data)
+
+export const updateGoal = (data) => Api.put("/update-goal",data)
