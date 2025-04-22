72c9991f0e9ef8cd970bf892a2e5d6127c70941f
Vishnu-raghav <vishnur1225@gmail.com>
2025-04-22 23:56:11 +0530

    create a login page

---
+++ Frontend/src/components/Login.jsx
@@ -0,0 +1,32 @@
+import { useState } from "react";
+import {useSelector,useDispatch} from "react-redux"
+import {useNavigate} from "react-router-dom"
+import {loginThunk} from "../features/auth/authThunk.js"
+import {Button} from "./Button.jsx"
+import Input from "./Input.jsx";
+import {useForm} from "react-hook-form"
+
+function Login(){
+    const Dispatch = useDispatch()
+    const navigate = useNavigate()
+
+    const {loading,error} = useSelector(state => state.auth)
+    const {register, handleSubmit} = useForm()
+    const [localError,setLocalError] = useState("")
+
+    const login = async (data) => {
+       setLocalError("")
+       try {
+        await Dispatch(loginThunk(data)).unwrap();
+        navigate('/')
+       } catch (error) {
+        setLocalError(error.message || "Login Failed")
+       }
+    }
+
+    return(
+        <>
+        
+        </>
+    )
+}
